8d934cbfd10c6c0995325f0471ec6877ebc065c1
Saniatul Haque Taposh <32695871+taposh99@users.noreply.github.com>
2023-02-26 12:13:39 +0600

    rate

--- resources/views/admin/home/home.blade.php
+++ resources/views/admin/home/home.blade.php
@@ -26,7 +26,7 @@
                         <div>
                             <p class="mb-0 text-secondary">Total Revenue</p>
                             <h4 class="my-1">$24K</h4>
-                            <p class="mb-0 font-13 text-success"><i class="bi bi-caret-up-fill"></i> 4.6 from last week</p>
+                            <p class="mb-0 font-13 text-success"><i class="bi bi-caret-up-fill"></i> 4.6 from week</p>
                         </div>
                         <div class="widget-icon-large bg-gradient-success text-white ms-auto"><i class="bi bi-currency-exchange"></i>
                         </div>
@@ -53,11 +53,7 @@
             <div class="card radius-10">
                 <div class="card-body">
                     <div class="d-flex align-items-center">
-                        <div>
-                            <p class="mb-0 text-secondary">Bounce Rate</p>
-                            <h4 class="my-1">38.15%</h4>
-                            <p class="mb-0 font-13 text-success"><i class="bi bi-caret-up-fill"></i> 12.2% from last week</p>
-                        </div>
+                       
                         <div class="widget-icon-large bg-gradient-info text-white ms-auto"><i class="bi bi-bar-chart-line-fill"></i>
                         </div>
                     </div>
